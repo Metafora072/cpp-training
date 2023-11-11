1c1fe71e9b63ba23fc961ad21b39a3126927ef45
Metafora <2430093500@qq.com>
2023-11-11 16:48:42 +0800

    passed should_return_100_when_given_cash_normal_and_price_100

--- tests/PriceCalculatorTest.cpp
+++ tests/PriceCalculatorTest.cpp
@@ -1,6 +1,12 @@
 #include <gtest/gtest.h>
 #include "PriceCalculator.h"
 
-TEST(hello, test_case1)
+TEST(PriceCalculator, should_return_100_when_given_cash_normal_and_price_100)
 {
+    // test1
+    PriceCal::PriceCalculator priceCalculator;
+
+    double cash = priceCalculator.calculatePrice(PriceCal::PriceCalculator::DiscountType::CASH_NORMAL, 100);
+
+    EXPECT_DOUBLE_EQ(100, cash);
 }
