2af278a54f69b6b71d5a6ff8da1d3aaad6233b31
Metafora <2430093500@qq.com>
2023-11-11 16:48:20 +0800

    complete PriceCalculator

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -5,5 +5,6 @@ namespace PriceCal {
         if(type == DiscountType::CASH_NORMAL) return price;
         if(type == DiscountType::CASH_TAKEOFF) return price * 0.9;
         if(type == DiscountType::CASH_DISCOUNT) return price - floor(price / 100) * 20;
+        return 0;
     }
 }
