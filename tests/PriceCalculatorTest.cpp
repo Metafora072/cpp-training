2ad5d81067bad5bb9e844b14568b5003e8f50a3f
Metafora <2430093500@qq.com>
2023-11-11 16:54:22 +0800

    passed should_return_90_when_given_cash_takeoff_and_price_100

--- tests/PriceCalculatorTest.cpp
+++ tests/PriceCalculatorTest.cpp
@@ -10,3 +10,13 @@ TEST(PriceCalculator, should_return_100_when_given_cash_normal_and_price_100)
 
     EXPECT_DOUBLE_EQ(100, cash);
 }
+
+TEST(PriceCalculator, should_return_90_when_given_cash_takeoff_and_price_100)
+{
+    // test2
+    PriceCal::PriceCalculator priceCalculator;
+
+    double cash = priceCalculator.calculatePrice(PriceCal::PriceCalculator::DiscountType::CASH_TAKEOFF, 100);
+
+    EXPECT_DOUBLE_EQ(90, cash);
+}
