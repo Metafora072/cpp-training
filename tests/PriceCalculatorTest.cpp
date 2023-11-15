17232cfe58faee50df4d5873271ac5791f27b9ec
Metafora <2430093500@qq.com>
2023-11-15 10:04:48 +0800

    passed should_return_80_when_given_cash_discount_and_price_100

--- tests/PriceCalculatorTest.cpp
+++ tests/PriceCalculatorTest.cpp
@@ -1,22 +1,42 @@
 #include <gtest/gtest.h>
 #include "PriceCalculator.h"
 
+using namespace PriceCal;
+
 TEST(PriceCalculator, should_return_100_when_given_cash_normal_and_price_100)
 {
-    // test1
-    PriceCal::PriceCalculator priceCalculator;
+    // given
+    PriceCalculator priceCalculator;
 
-    double cash = priceCalculator.calculatePrice(PriceCal::PriceCalculator::DiscountType::CASH_NORMAL, 100);
+    // when
+    double cash = priceCalculator.calculatePrice(PriceCalculator::DiscountType::CASH_NORMAL, 100);
 
+    // then
     EXPECT_DOUBLE_EQ(100, cash);
 }
 
 TEST(PriceCalculator, should_return_90_when_given_cash_takeoff_and_price_100)
 {
-    // test2
-    PriceCal::PriceCalculator priceCalculator;
+    // given
+    PriceCalculator priceCalculator;
 
-    double cash = priceCalculator.calculatePrice(PriceCal::PriceCalculator::DiscountType::CASH_TAKEOFF, 100);
+    // when
+    double cash = priceCalculator.calculatePrice(PriceCalculator::DiscountType::CASH_TAKEOFF, 100);
 
+    // then
     EXPECT_DOUBLE_EQ(90, cash);
 }
+
+
+TEST(PriceCalculator, should_return_80_when_given_cash_discount_and_price_100)
+{
+    // given
+    PriceCalculator priceCalculator;
+
+    // when
+    double cash = priceCalculator.calculatePrice(PriceCalculator::DiscountType::CASH_DISCOUNT, 100);
+
+    // then
+    EXPECT_DOUBLE_EQ(80, cash);
+}
+
