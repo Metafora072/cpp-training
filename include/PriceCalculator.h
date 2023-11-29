706fea402e3d361b3acc0476f3c887a9aea1b20b
Metafora <2430093500@qq.com>
2023-11-29 12:23:27 +0800

    添加Discount接口，将三种折扣类封装成PriceCalculator的子类

--- include/PriceCalculator.h
+++ include/PriceCalculator.h
@@ -9,57 +9,57 @@ enum class DiscountType
     CASH_PERCENTOFF_30,
     CASH_BACK,
 };
-
 namespace PriceCalc
 {
     class PriceCalculator final
     {
     public:
         double AcceptCash(const DiscountType discountType, const double money) const noexcept;
-    };
 
-    class Normal final
-    {
-    public:
-        double AcceptCash(const double money) const noexcept
-        {
-            return money;
-        }
-        virtual ~Normal();
-    };
-    class PercentOff final
-    {
     private:
-        double discountRate = 1;
-
-    public:
-        explicit PercentOff(double discountRate)
+        // 折扣接口，三种折扣必须实现该接口的AcceptCash方法
+        class Discount
         {
-            this->discountRate = discountRate;
-        }
-        virtual ~PercentOff();
+        public:
+            virtual double AcceptCash(const double money) const noexcept = 0;
+            virtual ~Discount();
+        };
 
-        double AcceptCash(const double money) const noexcept
+        class Normal final : public Discount
         {
-            return money * discountRate;
-        }
+        public:
+            double AcceptCash(const double money) const noexcept override
+            {
+                return money;
+            }
+            virtual ~Normal();
+        };
+
+        class PercentOff final : public Discount
+        {
+        private:
+            double discountRate = 1;
+
+        public:
+            explicit PercentOff(double rate) : discountRate(rate){};
+            virtual ~PercentOff();
+            double AcceptCash(const double money) const noexcept override
+            {
+                return money * discountRate;
+            }
+        };
+        class CashBack final : public Discount
+        {
+            double threshold = 100.0;
+            double cashback = 20.0;
+
+        public:
+            explicit CashBack(double threshold, double cashback) : threshold(threshold), cashback(cashback){};
+            virtual ~CashBack();
+            double AcceptCash(const double money) const noexcept override
+            {
+                return money - floor(money / threshold) * cashback;
+            }
+        };
     };
-    class CashBack final
-    {
-    private:
-        double threshold = 100.0;
-        double cashback = 20.0;
-
-    public:
-        explicit CashBack(double threshold, double cashback)
-        {
-            this->threshold = threshold;
-            this->cashback = cashback;
-        }
-        virtual ~CashBack();
-        double AcceptCash(const double money) const noexcept
-        {
-            return money - floor(money / threshold) * cashback;
-        }
-    };
-}
+} // namespace PriceCalc
--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -1,48 +1,53 @@
 #include "PriceCalculator.h"
 
 #include <cmath>
-
 namespace PriceCalc
 {
     double PriceCalculator::AcceptCash(const DiscountType discountType, const double money) const noexcept
     {
         double cash = 0.0;
 
-        switch (discountType) {
-            case DiscountType::CASH_NORMAL: {
-                cash = money;
-                break;
-            }
+        switch (discountType)
+        {
+        case DiscountType::CASH_NORMAL:
+        {
+            cash = money;
+            break;
+        }
 
-            case DiscountType::CASH_PERCENTOFF_10: {
-                const double discountRate = 0.9;
+        case DiscountType::CASH_PERCENTOFF_10:
+        {
+            const double discountRate = 0.9;
 
-                cash = money * discountRate;
-                break;
-            }
+            cash = money * discountRate;
+            break;
+        }
 
-            case DiscountType::CASH_PERCENTOFF_20: {
-                const double discountRate = 0.8;
+        case DiscountType::CASH_PERCENTOFF_20:
+        {
+            const double discountRate = 0.8;
 
-                cash = money * discountRate;
-                break;
-            }
+            cash = money * discountRate;
+            break;
+        }
 
-            case DiscountType::CASH_PERCENTOFF_30: {
-                const double discountRate = 0.7;
+        case DiscountType::CASH_PERCENTOFF_30:
+        {
+            const double discountRate = 0.7;
 
-                cash = money * discountRate;
-                break;
-            }
+            cash = money * discountRate;
+            break;
+        }
 
-            case DiscountType::CASH_BACK: {
-                const double threshold = 100.0;
-                const double cashback = 20.0;
+        case DiscountType::CASH_BACK:
+        {
+            const double threshold = 100.0;
+            const double cashback = 20.0;
 
-                cash = money - std::floor(money / threshold) * cashback;
+            cash = money - std::floor(money / threshold) * cashback;
 
-                break;
-            }
+            break;
+        }
         }
 
         return cash;
